360b002524d8f631ee65453c0b65549a5988cc3f
Granit Berisha <granit@hotelkeyapp.com>
2015-08-06 11:53:13 +0200

    demo

--- index.js
+++ index.js
@@ -60,12 +60,12 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+//WHERE $1 = Contact.id;', [request.params.id]
 
 //test for CRUD Json
 apiRoutes.get('/db3/:id', function(request, response){
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM Contact WHERE $1 = Contact.id;', [request.params.id], function(err, result) {
+		client.query('SELECT * FROM Contact ', function(err, result) {
 			done();
 			 if (err){ 
 				console.error(err); response.json({success:"false",message: err}); 
