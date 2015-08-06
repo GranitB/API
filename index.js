0bc19a09aac1b395ab02396a429eada6eda4108c
Granit Berisha <granit@hotelkeyapp.com>
2015-08-06 12:18:59 +0200

    demo

--- index.js
+++ index.js
@@ -76,6 +76,56 @@ apiRoutes.get('/db3', function(request, response){
 		});
 	});
 });
+
+apiRoutes.put('/db3/:id"', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.id;
+
+    // Grab data from http request
+    var data = {text: req.body.firstname};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+
+        // SQL Query > Update Data
+        client.query("UPDATE "Contact" SET firstname=($1) WHERE id=($3)", [data.text, id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM "Contact" ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
 /*
 app.get('/db', function (request, response) {
   
