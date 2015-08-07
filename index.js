5734b0566502201d237f72bfff535849d45fe41f
Granit Berisha <granit@hotelkeyapp.com>
2015-08-07 08:35:25 +0200

    demo

--- index.js
+++ index.js
@@ -101,19 +101,20 @@ apiRoutes.put('/db/:id', function(request, response){
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test WHERE $1 = test.id', [request.params.id], function(err, result) {
         var data1 = {name: request.body.name};  
-        //done();
+        done();
          if (err){ 
             console.error(err); response.json({success:"false", message: err}); 
          }
          else{  
-	//	 pg.connect(process.env.DATABASE_URL),function(err,client,done){
+		pg.connect(process.env.DATABASE_URL),function(err,client,done){
          client.query('UPDATE test SET name=($1) WHERE id=($2)', [data1.name], [request.params.id]);
             response.json({success:"true", data: result.rows} ); 
 			done();
          }
 		 
+		 
 		 }
-		 done();
+		// done();
 		 
     });
   });
